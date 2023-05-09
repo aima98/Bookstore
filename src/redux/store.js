7b71f3e3801b4a55fd6ae95327fad3e1b9be91cd
Oumaima Nejjari <oumanj1998@gmail.com>
2023-05-10 00:55:55 +0100

    Implementing required changes

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/prefer-default-export */
 import { configureStore } from '@reduxjs/toolkit';
 import booksReducer from './books/booksSlice';
 import categoriesReducer from './categories/categoriesSlice';
